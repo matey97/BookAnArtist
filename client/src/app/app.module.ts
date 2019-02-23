6123f0f1f47316a9638973c8c13af5a3370ad870
almevi <al341803@uji.es>
2019-02-23 18:33:11 +0100

    Probando Primer commit + Cosas de Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import {MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolba
     MatCardModule,
     MatInputModule,
     MatListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
