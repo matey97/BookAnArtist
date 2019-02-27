ce6ac36b890553a21b0c11b4bda9a7a251bc227b
Sergio Jimenez <al341933@uji.es>
2019-02-27 15:46:55 +0100

    Añade conexión con el fichero proxy, script para arrancar con proxy usando npm start

--- server/src/main/java/com/ei104550/BookAnArtist/repositories/ArtistRepository.java
+++ server/src/main/java/com/ei104550/BookAnArtist/repositories/ArtistRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 }
