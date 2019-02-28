21bb18108b867b681ab08dee87c2b7a76bfc8ab6
almevi <al341803@uji.es>
2019-02-28 16:37:00 +0100

    Ruta y diseño general artistList

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArtistListComponent } from './artist-list/artist-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'artistList', component: ArtistListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
